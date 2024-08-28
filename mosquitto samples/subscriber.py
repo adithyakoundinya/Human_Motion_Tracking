886bf8c58643645fee1826195907e529e34aedfb
Aakash Negi <anegi2002@gmail.com>
2024-08-28 14:24:30 +0530

    outro commit

---
+++ mosquitto samples/subscriber.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+A small example subscriber
+"""
+import paho.mqtt.client as paho
+
+def on_message(mosq, obj, msg):
+    print(msg.topic, msg.qos, msg.payload)
+    #  "%-20s %d %s" % 
+    mosq.publish('pong', 'ack', 0)
+
+def on_publish(mosq, obj, mid):
+    pass
+
+if __name__ == '__main__':
+    client = paho.Client()
+    client.on_message = on_message
+    client.on_publish = on_publish
+
+    #client.tls_set('root.ca', certfile='c1.crt', keyfile='c1.key')
+    client.connect("127.0.0.1", 1883, 60)
+
+    client.subscribe("kids/yolo", 0)
+    client.subscribe("sample/#", 0)
+
+    while client.loop() == 0:
+        pass
+
+# vi: set fileencoding=utf-8 :
